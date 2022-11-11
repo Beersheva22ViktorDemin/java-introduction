8cd9b87c2ca0159d031c5c1080c6fb549ac5384d
Victor Demin <glendemon@gmail.com>
2022-11-11 12:32:38 +0200

    from lesson;

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -57,5 +57,51 @@ public class MyArrays {
 		
 		return result;
 	}
+	
+	/**
+	 * 
+	 * @param arraySorted - sorted array
+	 * @param number
+	 * @return new array with inserted number at an index for keeping array sorted
+	 */
+	public static int[] insertSorted(int arraySorted[], int number) {
+		// use the method binarySearch of the standard class Arrays
+		// use the method arraycopy of the standard class System
+		int index = Arrays.binarySearch(arraySorted, number);
+		if (index < 0) {
+			index = -(index + 1);
+		}
+		return insertAtIndex(arraySorted, number, index);
+		
+	}
+	private static int[] insertAtIndex(int[] array, int number, int index) {
+		int res[] = new int[array.length + 1];
+		System.arraycopy(array, 0, res, 0, index);
+		res[index] = number;
+		System.arraycopy(array, index, res, index + 1, array.length - index);
+		return res;
+	}
+	/**
+	 * 
+	 * @param arraySorted
+	 * @param number
+	 * @return index value if number exists otherwise -1
+	 * O[N] - search number in unsorted array
+	 * O[LogN] - search number in sorted (binary search)
+	 */
+	public static int binarySearch(int arraySorted[], int number) {
+		int left = 0;
+		int right = arraySorted.length - 1;
+		int middle = right / 2;
+		while(left <= right && arraySorted[middle] != number) {
+			if (number < arraySorted[middle]) {
+				right = middle - 1;
+			} else {
+				left = middle + 1;
+			}
+			middle = (left + right) / 2;
+		}
+		return left > right ? -1 : middle;
+	}
 }
 
