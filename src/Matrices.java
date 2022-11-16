77f27eb5286b27e072e6cd0eeee169f1dd678c36
Victor Demin <glendemon@gmail.com>
2022-11-16 16:05:40 +0200

    matrix transp;

--- src/Matrices.java
+++ src/Matrices.java
@@ -6,12 +6,20 @@ public class Matrices {
 				res[i][j] = (int) Numbers.getRandomNumber(minValue, maxValue);
 			}
 		}
+		
 		return res;
 	}
 
 	public static int[][] transp(int[][] matrix) {
-		// TODO
-		return null;
+		int rows = matrix.length;
+		int columns = rows > 1 ? matrix[0].length : 0;
+		int result[][] = new int[columns][rows];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				result[j][i] = matrix[i][j];
+			}
+		}
+		
+		return result;
 	}
-
 }
