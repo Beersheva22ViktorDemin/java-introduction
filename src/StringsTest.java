4f16bd91b9db0764eaac320396f50be891bc8281
Victor Demin <glendemon@gmail.com>
2022-11-19 10:47:59 +0200

    add more tests for isAnagram;

--- src/StringsTest.java
+++ src/StringsTest.java
@@ -5,9 +5,20 @@ import org.junit.jupiter.api.Test;
 class StringsTest {
 
 	@Test
-	void test() {
+	void isAnagramTest() {
 		assertTrue(Strings.isAnagram("anagram", "gramana"));
 		assertFalse(Strings.isAnagram("anagram", "graman"));
 		assertFalse(Strings.isAnagram("anagram", "gramans"));
+
+		String word = "yellow";
+		assertTrue(Strings.isAnagram(word, "loweyl"));
+		assertTrue(Strings.isAnagram(word, "elolyw"));
+		assertTrue(Strings.isAnagram(word, "wolley"));
+		assertTrue(Strings.isAnagram(word, "loleyw"));
+		assertFalse(Strings.isAnagram(word, ""));
+		assertFalse(Strings.isAnagram(word, "yellob"));
+		assertFalse(Strings.isAnagram(word, "yello"));
+		assertFalse(Strings.isAnagram(word, "yelllo"));
+
 	}
 }
